f204e6316c66fe849f56db9bd0391364eeba0ea1
sachingawde <sachingawde88@gmail.com>
2021-10-07 00:56:56 +0530

    day12 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:firstapp/models/catalog.dart';
 import 'package:firstapp/widgets/drawer.dart';
+import 'package:firstapp/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,14 +10,20 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("Finance App"),
       ),
-      body: Center(
-        child: Container(
-          child: Text(
-              "Welcome to $days of flutter by $name"), //string interpolation- $days
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
